561f7c534b020a4ba2fa784c93e7774796dc69c9
Michail <astapenko.m99@gmail.com>
2024-12-22 21:51:22 +0300

    feat: done 40 tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -465,8 +465,9 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  const arr = numbers.filter((num) => num % 2 !== 0);
+  return arr.map((el) => numbers.indexOf(el));
 }
 
 /**
